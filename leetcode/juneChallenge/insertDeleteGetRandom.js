4f2f0951cf969e2f888601d7d17a203b215272b4
Mridul Gupta <48939591+mridulgupta210@users.noreply.github.com>
2020-06-12 23:03:23 +0530

    Create insertDeleteGetRandom.js

---
+++ leetcode/juneChallenge/insertDeleteGetRandom.js
@@ -0,0 +1,96 @@
+/*
+Design a data structure that supports all following operations in average O(1) time.
+
+insert(val): Inserts an item val to the set if not already present.
+remove(val): Removes an item val from the set if present.
+getRandom: Returns a random element from current set of elements. Each element must have the same probability of being returned.
+Example:
+
+// Init an empty set.
+RandomizedSet randomSet = new RandomizedSet();
+
+// Inserts 1 to the set. Returns true as 1 was inserted successfully.
+randomSet.insert(1);
+
+// Returns false as 2 does not exist in the set.
+randomSet.remove(2);
+
+// Inserts 2 to the set, returns true. Set now contains [1,2].
+randomSet.insert(2);
+
+// getRandom should return either 1 or 2 randomly.
+randomSet.getRandom();
+
+// Removes 1 from the set, returns true. Set now contains [2].
+randomSet.remove(1);
+
+// 2 was already in the set, so return false.
+randomSet.insert(2);
+
+// Since 2 is the only number in the set, getRandom always return 2.
+randomSet.getRandom();
+*/
+
+/**
+ * Initialize your data structure here.
+ */
+var RandomizedSet = function() {
+    this.map = {};
+    this.arr = [];
+};
+
+/**
+ * Inserts a value to the set. Returns true if the set did not already contain the specified element. 
+ * @param {number} val
+ * @return {boolean}
+ */
+RandomizedSet.prototype.insert = function(val) {
+    if (this.map.hasOwnProperty(val)) {
+        return false;
+    }
+    
+    this.map[val] = this.arr.length;
+    this.arr.push(val);
+    return true;
+};
+
+/**
+ * Removes a value from the set. Returns true if the set contained the specified element. 
+ * @param {number} val
+ * @return {boolean}
+ */
+RandomizedSet.prototype.remove = function(val) {
+    if (!this.map.hasOwnProperty(val)) {
+        return false;
+    }
+    
+
+    if (this.map[val] !== this.arr.length - 1) {
+        this.map[this.arr[this.arr.length - 1]] = this.map[val];
+        
+        // swap
+        const temp = this.arr[this.map[val]];
+        this.arr[this.map[val]] = this.arr[this.arr.length - 1];
+        this.arr[this.arr.length - 1] = temp;
+    }
+
+    this.arr.pop();
+    delete this.map[val];
+    return true;
+};
+
+/**
+ * Get a random element from the set.
+ * @return {number}
+ */
+RandomizedSet.prototype.getRandom = function() {
+    return this.arr[Math.floor(Math.random() * this.arr.length)];
+};
+
+/** 
+ * Your RandomizedSet object will be instantiated and called as such:
+ * var obj = new RandomizedSet()
+ * var param_1 = obj.insert(val)
+ * var param_2 = obj.remove(val)
+ * var param_3 = obj.getRandom()
+ */
